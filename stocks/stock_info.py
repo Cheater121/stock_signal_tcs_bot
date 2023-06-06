314a0d4bae5774c7d62dad59affaa0915cbb5d4c
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-06-06 13:26:43 +0300

    Update indicators

--- stocks/stock_info.py
+++ stocks/stock_info.py
@@ -8,7 +8,7 @@ from strategies.macd import get_macd
 from strategies.moving_averages import get_ma
 from strategies.interval_levels import get_interval_levels
 from config_data.config import load_config
-from models.models import db, Stock, Indicator
+
 
 config = load_config()
 
@@ -16,35 +16,20 @@ TCS_TOKEN = config.tcs_client.token
 
 
 class StockAnalyzer:
-    # Support/resistance levels Strategy
-    old_levels = {'PRICE': 0, 'MA20': 0, 'MA50': 0, 'MA100': 0, 'MA200': 0, 'YESTERDAY_LOW': 0, 'YESTERDAY_HIGH': 0,
-                  'WEEK_LOW': 0, 'WEEK_HIGH': 0, 'MONTH_LOW': 0, 'MONTH_HIGH': 0}
-    levels = {'PRICE': 0, 'MA20': 0, 'MA50': 0, 'MA100': 0, 'MA200': 0, 'YESTERDAY_LOW': 0, 'YESTERDAY_HIGH': 0,
-              'WEEK_LOW': 0, 'WEEK_HIGH': 0, 'MONTH_LOW': 0, 'MONTH_HIGH': 0}
-
-    # RSI Strategy
-    old_rsi = None
-    current_rsi = None
-
-    # MACD Strategy
-    old_macd = None
-    old_macds = None
-    macd = None
-    macds = None
-
-    # SMA 20/50 Strategy
-    old_ma20_hour = None
-    old_ma50_hour = None
-    ma20_hour = None
-    ma50_hour = None
 
     def __init__(self, figi: str, ticker: str):
         self.figi = figi
         self.ticker = ticker
+        self.old_levels = {"PRICE": 0, "MA20": 0, "MA50": 0, "MA100": 0, "MA200": 0, "YESTERDAY_LOW": 0, "YESTERDAY_HIGH": 0,
+              "WEEK_LOW": 0, "WEEK_HIGH": 0, "MONTH_LOW": 0, "MONTH_HIGH": 0, 
+              "RSI": None, "MACD": None, "MACDs": None, "MA20_HOUR": None, "MA50_HOUR": None}
+        self.levels = {"PRICE": 0, "MA20": 0, "MA50": 0, "MA100": 0, "MA200": 0, "YESTERDAY_LOW": 0, "YESTERDAY_HIGH": 0,
+              "WEEK_LOW": 0, "WEEK_HIGH": 0, "MONTH_LOW": 0, "MONTH_HIGH": 0, 
+              "RSI": None, "MACD": None, "MACDs": None, "MA20_HOUR": None, "MA50_HOUR": None}
 
     def get_new_prices(self, interval=CandleInterval.CANDLE_INTERVAL_DAY, days=300):
         try:
-            timeframe = 'HOUR' if interval == CandleInterval.CANDLE_INTERVAL_HOUR else 'DAY'
+            timeframe = "HOUR" if interval == CandleInterval.CANDLE_INTERVAL_HOUR else "DAY"
             with Client(TCS_TOKEN) as client:
                 close_prices = []
                 minimal_values = []
@@ -61,73 +46,28 @@ class StockAnalyzer:
         except Exception as e:
             logger.exception(f"Exception in get prices method: \n{e}\n")
 
-    def _update_prices(self, close_price, close_prices, minimal_values, maximum_values, timeframe='DAY'):
+    def _update_prices(self, close_price, close_prices, minimal_values, maximum_values, timeframe="DAY"):
         try:
-            if timeframe == 'DAY':
+            if timeframe == "DAY":
                 ma20, ma50, ma100, ma200 = get_ma(close_prices)
                 month_low, month_high, week_low, week_high, prev_day_low, prev_day_high = get_interval_levels(
                     minimal_values, maximum_values)
-                print(f'{self.ticker} MA20 = {ma20}', f'MA50 = {ma50}',
-                      f'MA100 = {ma100}', f'MA200 = {ma200}', f'Price = {close_price}',
-                      f'Previous day low = {prev_day_low}', f'Seven day low = {week_low}',
-                      f'Month low = {month_low}', f'Previous day high = {prev_day_high}',
-                      f'Seven day high = {week_high}', f'Month high = {month_high}')
-                self.levels = {'PRICE': close_price, 'MA20': round(ma20, 2),
-                               'MA50': round(ma50, 2), 'MA100': round(ma100, 2),
-                               'MA200': round(ma200, 2), 'YESTERDAY_LOW': prev_day_low,
-                               'YESTERDAY_HIGH': prev_day_high, 'WEEK_LOW': week_low, 'WEEK_HIGH': week_high,
-                               'MONTH_LOW': month_low, 'MONTH_HIGH': month_high}
-                self.current_rsi = get_current_rsi(close_prices)
-                self.macd, self.macds = get_macd(close_prices)
-            elif timeframe == 'HOUR':
-                self.ma20_hour, self.ma50_hour, *_ = get_ma(close_prices)
-                print(f'SMA20: {self.ma20_hour}, SMA50: {self.ma50_hour}')
+                rsi = get_current_rsi(close_prices)
+                macd, macds = get_macd(close_prices)
+                self.levels = {"PRICE": close_price, "MA20": round(ma20, 2),
+                               "MA50": round(ma50, 2), "MA100": round(ma100, 2),
+                               "MA200": round(ma200, 2), "YESTERDAY_LOW": prev_day_low,
+                               "YESTERDAY_HIGH": prev_day_high, "WEEK_LOW": week_low, "WEEK_HIGH": week_high,
+                               "MONTH_LOW": month_low, "MONTH_HIGH": month_high, "RSI": rsi, "MACD": macd, "MACDs": macds}
+                print(f"{self.ticker} {self.levels}")
+            elif timeframe == "HOUR":
+                self.levels["MA20_HOUR"], self.levels["MA50_HOUR"], *_ = get_ma(close_prices)
+                print("Hour MA successfully added")
             else:
                 raise AssertionError
         except Exception as e:
             logger.exception(f"Exception in update prices method: \n{e}\n")
 
-    def load_old_prices(self): # ToDo write this method
-        try:
-            pass
-        except Exception as e:
-            logger.exception(f"Exception in load old prices method: \n{e}\n")
-
-    def save_old_prices(self):
-        try:
-            db.create_tables([Stock, Indicator])
-        except Exception as e:
-            logger.exception(f"Exception in create tables method: \n{e}\n")
-
-        try:
-            stock = Stock.select().where(Stock.ticker == self.ticker)
-        except Exception as e:
-            stock = Stock(ticker=self.ticker, figi=self.figi)
-            stock.save()
-            logger.exception(f"Exception in save stock method: \n{e}\n")
-
-        try:
-            indicators = Indicator(price=self.old_levels.get("PRICE"),
-                                   from_stock=stock,
-                                   ma20=self.old_levels.get("MA20"),
-                                   ma50=self.old_levels.get("MA50"),
-                                   ma100=self.old_levels.get("MA100"),
-                                   ma200=self.old_levels.get("MA200"),
-                                   yesterday_low=self.old_levels.get("YESTERDAY_LOW"),
-                                   yesterday_high=self.old_levels.get("YESTERDAY_HIGH"),
-                                   week_low=self.old_levels.get("WEEK_LOW"),
-                                   week_high=self.old_levels.get("WEEK_HIGH"),
-                                   month_low=self.old_levels.get("MONTH_LOW"),
-                                   month_high=self.old_levels.get("MONTH_HIGH"),
-                                   rsi=self.old_rsi,
-                                   macd=self.old_macd,
-                                   macds=self.old_macds,
-                                   ma20_hour=self.old_ma20_hour,
-                                   ma50_hour=self.old_ma50_hour,
-                                   date=datetime.now())
-            indicators.save()
-        except Exception as e:
-            logger.exception(f"Exception in save old prices (indicators) method: \n{e}\n")
 
 
 ozon = StockAnalyzer("BBG00Y91R9T3", "OZON")
@@ -153,3 +93,4 @@ moex = StockAnalyzer("BBG004730JJ5", "MOEX")
 
 stocks_list = [ozon, sber, sgzh, poly, vkco, tatn, nvtk, spbe, nlmk, pikk, five, afks, yndx, rosn, alrs, gmkn, aflt,
                gazp, lkoh, moex]
+               
